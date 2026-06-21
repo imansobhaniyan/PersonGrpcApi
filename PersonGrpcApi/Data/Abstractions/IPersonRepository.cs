bf306777127e5fc5a45ced4c797403564d9a818e
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2026-06-21 18:01:20 +0330

    add repository abstraction

---
+++ PersonGrpcApi/Data/Abstractions/IPersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using PersonGrpcApi.Models;
+
+namespace PersonGrpcApi.Data.Abstractions;
+
+public interface IPersonRepository
+{
+    Task<Person> GetByIdAsync(int id);
+
+    Task<List<Person>> GetAllAsync();
+
+    Task<bool> ExistsAsync(string nationalCode);
+
+    Task<Person> CreateAsync(string firstName, string lastName, string nationalCode, DateTime? birthDate);
+
+    Task<Person> UpdateAsync(int id, string firstName, string lastName, string nationalCode, DateTime? birthDate);
+
+    Task<bool> DeleteAsync(int id);
+}
