2948e0e0a416c59e93e17a474794beaebf13e36b
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2026-06-21 17:11:24 +0330

    add person model

---
+++ PersonGrpcApi/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace PersonGrpcApi.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public required string NationalCode { get; set; }
+
+    public DateTime? BirthDate { get; set; }
+}
