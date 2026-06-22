1f65e00b208a20190e2c1e7a9a4fd4b116802e5d
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2026-06-22 23:16:58 +0330

    add PersonResponse partial class to have ctor with person input

---
+++ PersonGrpcApi/Protos/PersonResponse.cs
@@ -0,0 +1,18 @@
+﻿
+//changed namespace to match the actual auto generated class
+using PersonGrpcApi.Models;
+
+namespace PersonGrpcApi.GrpcModels
+{
+    public partial class PersonResponse
+    {
+        public PersonResponse(Person person)
+        {
+            Id = person.Id;
+            FirstName = person.FirstName;
+            LastName = person.LastName;
+            NationalCode = person.NationalCode;
+            BirthDate = person.BirthDate?.ToString("yyyy-MM-dd");
+        }
+    }
+}
