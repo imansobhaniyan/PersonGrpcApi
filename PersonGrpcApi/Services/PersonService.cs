7a17c7ca2f4702b1b07866cc317ea76dde1122a1
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2026-06-22 23:12:45 +0330

    inject unit of work into person service

--- PersonGrpcApi/Services/PersonService.cs
+++ PersonGrpcApi/Services/PersonService.cs
@@ -1,8 +1,15 @@
-﻿using PersonGrpcApi.GrpcModels;
+﻿using PersonGrpcApi.Data.Abstractions;
+using PersonGrpcApi.GrpcModels;
 
 namespace PersonGrpcApi.Services
 {
     public class PersonService : PersonProtoService.PersonProtoServiceBase
     {
+        private readonly IUnitOfWork unitOfWork;
+
+        public PersonService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
     }
 }
