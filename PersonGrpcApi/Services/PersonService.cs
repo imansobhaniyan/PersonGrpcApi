c3e80588bac4c10fcb477866385fb950cfa02ca6
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2026-06-23 00:23:13 +0330

    implement update method in person service

--- PersonGrpcApi/Services/PersonService.cs
+++ PersonGrpcApi/Services/PersonService.cs
@@ -41,14 +41,7 @@ namespace PersonGrpcApi.Services
 
         public override async Task<PersonResponse> Create(CreatePersonRequest request, ServerCallContext context)
         {
-            if (string.IsNullOrWhiteSpace(request.FirstName?.Trim()))
-                throw new RpcException(new Status(StatusCode.InvalidArgument, "First Name is required"));
-
-            if (string.IsNullOrWhiteSpace(request.LastName?.Trim()))
-                throw new RpcException(new Status(StatusCode.InvalidArgument, "Last Name is required"));
-
-            if (string.IsNullOrWhiteSpace(request.NationalCode?.Trim()))
-                throw new RpcException(new Status(StatusCode.InvalidArgument, "National Code is required"));
+            validateArguments(request.FirstName, request.LastName, request.NationalCode);
 
             if (!NationalCodeValidator.Validate(request.NationalCode))
                 throw new RpcException(new Status(StatusCode.InvalidArgument, "National Code is invalid"));
@@ -56,9 +49,30 @@ namespace PersonGrpcApi.Services
             if (await unitOfWork.PersonRepository.ExistsAsync(request.NationalCode))
                 throw new RpcException(new Status(StatusCode.AlreadyExists, "National Code already exists"));
 
-            DateTime? birthDate = DateTime.TryParseExact(request.BirthDate, PersonResponse.BIRTH_DATE_FORMAT, null, DateTimeStyles.None, out var res) ? res : null;
+            DateTime? birthDate = parseBirthDate(request.BirthDate);
 
             var person = await unitOfWork.PersonRepository.CreateAsync(request.FirstName, request.LastName, request.NationalCode, birthDate);
+
+            await unitOfWork.CommitAsync();
+
+            return new PersonResponse(person);
+        }
+
+        public override async Task<PersonResponse> Update(UpdatePersonRequest request, ServerCallContext context)
+        {
+            validateArguments(request.FirstName, request.LastName, request.NationalCode);
+
+            var person = await unitOfWork.PersonRepository.GetByIdAsync(request.Id);
+
+            if (person == null)
+                throw new RpcException(new Status(StatusCode.NotFound, "Person not found"));
+
+            if (person.NationalCode != request.NationalCode)
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "National Code could not be changed"));
+
+            DateTime? birthDate = parseBirthDate(request.BirthDate);
+
+            person = await unitOfWork.PersonRepository.UpdateAsync(request.Id, request.FirstName, request.LastName, request.NationalCode, birthDate);
 
             await unitOfWork.CommitAsync();
 
@@ -73,6 +87,23 @@ namespace PersonGrpcApi.Services
                 throw new RpcException(new Status(StatusCode.NotFound, "Person not found"));
 
             return new Empty();
+        }
+
+        private static DateTime? parseBirthDate(string birthDate)
+        {
+            return DateTime.TryParseExact(birthDate, PersonResponse.BIRTH_DATE_FORMAT, null, DateTimeStyles.None, out var res) ? res : null;
+        }
+
+        private static void validateArguments(string firstName, string lastName, string nationalCode)
+        {
+            if (string.IsNullOrWhiteSpace(firstName?.Trim()))
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "First Name is required"));
+
+            if (string.IsNullOrWhiteSpace(lastName?.Trim()))
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Last Name is required"));
+
+            if (string.IsNullOrWhiteSpace(nationalCode?.Trim()))
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "National Code is required"));
         }
     }
 }
