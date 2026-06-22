c8362238218db0a129ffd485208f9600f491e428
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2026-06-23 00:48:56 +0330

    init ef db context and repository

---
+++ PersonGrpcApi/Data/EF/PersonDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using PersonGrpcApi.Data.Abstractions;
+using PersonGrpcApi.Models;
+
+namespace PersonGrpcApi.Data.EF
+{
+    public class PersonDbContext : DbContext, IUnitOfWork
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options)
+        {
+            PersonRepository = new PersonRepository(this);
+        }
+
+        public DbSet<Person> People { get; set; }
+
+        public IPersonRepository PersonRepository { get; }
+
+        public async Task CommitAsync()
+        {
+            await SaveChangesAsync();
+        }
+    }
+}
---
+++ PersonGrpcApi/Data/EF/PersonRepository.cs
@@ -0,0 +1,45 @@
+﻿using PersonGrpcApi.Data.Abstractions;
+using PersonGrpcApi.Models;
+
+namespace PersonGrpcApi.Data.EF
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly PersonDbContext dbContext;
+
+        public PersonRepository(PersonDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task<Person> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Person>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsAsync(string nationalCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Person> CreateAsync(string firstName, string lastName, string nationalCode, DateTime? birthDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Person> UpdateAsync(int id, string firstName, string lastName, string nationalCode, DateTime? birthDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
